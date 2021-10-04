7d47b5a8ef1a4920a87f2ce2b72ee88d7307c7e4
郵差 <a34007024@gmail.com>
2021-10-04 18:35:30 +0800

    Create factors_prime_numbers_and_least_common_multiple.php

---
+++ Week1/factors_prime_numbers_and_least_common_multiple.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>因數質數與最小公倍數計算機</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
