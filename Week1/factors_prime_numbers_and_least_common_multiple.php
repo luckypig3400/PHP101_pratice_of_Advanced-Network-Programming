149c08597685d7d35d2f18371c3897ee588435d0
郵差 <a34007024@gmail.com>
2021-10-05 14:14:56 +0800

    check input error and set error css

--- Week1/factors_prime_numbers_and_least_common_multiple.php
+++ Week1/factors_prime_numbers_and_least_common_multiple.php
@@ -11,7 +11,7 @@
 
 <body>
     <div class="container">
-        <form action="./factors_prime_numbers_and_least_common_multiple.php">
+        <form action="./factors_prime_numbers_and_least_common_multiple.php" method="POST">
             <div class="row">
                 <h3>本程式會自動計算A、B兩數的各自的因數、最大公因數、最小公倍數</h3>
             </div>
@@ -36,6 +36,28 @@
                 <input type="submit" value="開始計算">
             </div>
         </form>
+
+        <br><br><br>
+        <?php
+        //輸出結果參考
+        /*3 是質數 因數有：3 / 1 /
+        69369 是合數 因數有：69369 / 23123 / 3651 / 1217 / 57 / 19 / 3 / 1 /
+        3 與 69369 兩數不互質 公因數有：3 / 1 /
+        兩數最大公因數：3
+        兩數最小公倍數：69369
+        */
+        if (isset($_POST['numberA']) && isset($_POST['numberB'])) {
+            $a = $_POST['numberA'];
+            $b = $_POST['numberB'];
+
+            if ($a == "" || $b == "") {
+                echo "<div class=\"row\"><div class=\"error\">有數字尚未輸入，請檢查!</div></div>";
+            } else if ((int)$a <= 0 || (int)$b <= 0) {
+                echo "<div class=\"row\"><div class=\"error\">輸入的數字小於或等於0 請改為輸入大於的數字喔!</div></div>";
+            } else echo "接收到的數字A:$a , 數字B:$b";
+        }
+        ?>
+
     </div>
 </body>
 
