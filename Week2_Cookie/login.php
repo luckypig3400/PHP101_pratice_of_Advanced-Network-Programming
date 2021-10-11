092e0fc24cdd32f50f8cbf0e5bc80002b9cda02c
郵差 <a34007024@gmail.com>
2021-10-11 21:42:07 +0800

    Create login.php

---
+++ Week2_Cookie/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login--Cookie Ver</title>
+    <link rel="stylesheet" href="../general_style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <h1>Login--Cookie Ver</h1>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="acc">請輸入帳號:</label>
+            </div>
+            <div class="col-50">
+                <input type="text" name="account" id="acc" placeholder="Your Account">
+            </div>
+            <div class="col-25">
+                <input type="submit" value="登入">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
