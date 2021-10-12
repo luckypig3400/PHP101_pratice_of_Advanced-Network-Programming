ef6a92d155749d1a50cd9dfad3afb713eb518c1b
郵差 <a34007024@gmail.com>
2021-10-12 14:56:55 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+//Button shape link:
+//https://stackoverflow.com/questions/2906582/how-to-create-an-html-button-that-acts-like-a-link
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>首頁--進階網路程式設計作業選單</title>
+    <link rel="stylesheet" href="./general_style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1 align="center">大三上 PHP1101 進階網路程式設計作業選單</h1>
+
+        <div class="row">
+            <div class="col-50">
+                <h3>Week1 質因數計算機</h3>
+            </div>
+            <form action="./Week1/factors_prime_numbers_and_least_common_multiple.php">
+                <div class="col-50">
+                    <input type="submit" value="點我前往">
+                </div>
+            </form>
+        </div>
+        <br><br><br>
+        <div class="row">
+            <div class="col-50">
+                <h3>Week2 簡易登入系統(Cookie版本)</h3>
+            </div>
+            <form action="./Week2_Cookie/login.php">
+                <div class="col-50">
+                    <input type="submit" value="點我前往">
+                </div>
+            </form>
+        </div>
+
+    </div>
+</body>
+
+</html>
