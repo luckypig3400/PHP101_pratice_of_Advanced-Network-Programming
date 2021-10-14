918134dcbec717f61a947dcdf115166f303fadb2
郵差 <a34007024@gmail.com>
2021-10-14 22:41:06 +0800

    redirect to login page if input illegal

--- Week3_CSS_JS/js/week3_input_limiter.js
+++ Week3_CSS_JS/js/week3_input_limiter.js
@@ -8,11 +8,11 @@ function account_input_check() {
     
     if (accInput == null || accInput == "") {
         alert("Please Fill the Account Field");
-        returnToPreviousPage()
+        returnToPreviousPage();
         return false;
     }else if(accInput.length < 4 || accInput.length >10){
         alert("Account Length should between 4~10 characters");
-        returnToPreviousPage()
+        returnToPreviousPage();
         return false;
     }else{
         return true;
@@ -23,5 +23,6 @@ function account_input_check() {
 //stop form post request
 //https://stackoverflow.com/questions/8664486/javascript-code-to-stop-form-submission
 function returnToPreviousPage(){
+    window.location.replace("../login.php");
     window.history.back();
 }
