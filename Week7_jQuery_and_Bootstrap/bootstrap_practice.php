ce381f1e01c760024ae042b662e43f5f02a4e233
郵差 <twHenryYang@gmail.com>
2021-11-11 14:00:15 +0800

    Format file

--- Week7_jQuery_and_Bootstrap/bootstrap_practice.php
+++ Week7_jQuery_and_Bootstrap/bootstrap_practice.php
@@ -1,64 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Bootstrap Practice</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Practice</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
 
-    <body>
-        <h1>Bootstrap Playground</h1>
+<body>
+    <h1>Bootstrap Playground</h1>
 
-        <br>
-        <h3>Badges</h3><br>
-        <!-- https://getbootstrap.com/docs/5.1/components/badge/ -->
-        <button type="button" class="btn btn-primary">
-            首頁
-        </button>
-        <a href="https://tw.yahoo.com/?p=us">
-            <button type="button" class="btn btn-info">
-                Yahoo通知
-                <span class="badge bg-secondary">
-                    <?php
-                    echo rand(1,69);
-                    ?>
-                </span>
-            </button>
-        </a>
-        <button type="button" class="btn btn-outline-info position-relative">
-            廣告
-            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                99+
-                <span class="visually-hidden">unread messages</span>
+    <br>
+    <h3>Badges</h3><br>
+    <!-- https://getbootstrap.com/docs/5.1/components/badge/ -->
+    <button type="button" class="btn btn-primary">
+        首頁
+    </button>
+    <a href="https://tw.yahoo.com/?p=us">
+        <button type="button" class="btn btn-info">
+            Yahoo通知
+            <span class="badge bg-secondary">
+                <?php
+                echo rand(1, 69);
+                ?>
             </span>
         </button>
-        <button type="button" class="btn btn-outline-info">
-            我的帳號
-        </button>
-        <button type="button" class="btn btn-danger">
-            登出
-        </button>
-        <br><br><br>
-        
-        <h3>Progress Bar</h3>
-        <br><br><br>
-        <!-- https://getbootstrap.com/docs/5.1/components/progress/ -->
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 15%" aria-valuenow="15" aria-valuemin="0"
-                aria-valuemax="100"></div>
-            <div class="progress-bar bg-success" role="progressbar" style="width: 30%" aria-valuenow="30"
-                aria-valuemin="0" aria-valuemax="100"></div>
-            <div class="progress-bar bg-info" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0"
-                aria-valuemax="100"></div>
-        </div>
+    </a>
+    <button type="button" class="btn btn-outline-info position-relative">
+        廣告
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            99+
+            <span class="visually-hidden">unread messages</span>
+        </span>
+    </button>
+    <button type="button" class="btn btn-outline-info">
+        我的帳號
+    </button>
+    <button type="button" class="btn btn-danger">
+        登出
+    </button>
+    <br><br><br>
 
-    </body>
+
+
+</body>
 
 </html>
