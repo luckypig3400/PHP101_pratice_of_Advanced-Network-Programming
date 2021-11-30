aecf8a053106ee3e4d63661759efc1430f6982ce
郵差 <twHenryYang@gmail.com>
2021-11-30 16:05:25 +0800

    fix: change javascript import sequence

--- Week10_FileUpload_and_ImageRWD/imageUploader.php
+++ Week10_FileUpload_and_ImageRWD/imageUploader.php
@@ -8,12 +8,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>圖片上傳器與響應式相簿</title>
-    <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-    <script src="../bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../jquery-3.6.0.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="./script.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
