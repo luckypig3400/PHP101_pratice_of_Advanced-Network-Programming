d32ea3f7678442dd48450259ddc604a77ebe531a
郵差 <twHenryYang@gmail.com>
2021-11-30 16:59:31 +0800

    modify some text & style

--- Week10_FileUpload_and_ImageRWD/imageUploader.php
+++ Week10_FileUpload_and_ImageRWD/imageUploader.php
@@ -25,21 +25,21 @@
                     <div class="d-flex" id="wrapper">
                         <!-- Sidebar -->
                         <div class="bg-light border-right" id="sidebar-wrapper">
-                            <div class="sidebar-heading pt-5 pb-4"><strong>PAY WITH</strong></div>
-                            <div class="list-group list-group-flush"> <a data-toggle="tab" href="#menu1" id="tab1" class="tabs list-group-item bg-light">
+                            <div class="sidebar-heading pt-5 pb-4"><strong>圖片上傳器與<br>響應式相簿</strong></div>
+                            <div class="list-group list-group-flush">
+                                <a data-toggle="tab" href="#menu2" id="tab2" class="tabs list-group-item active1">
                                     <div class="list-div my-2">
-                                        <div class="fa fa-home"></div> &nbsp;&nbsp; Bank
+                                        <div class="fa fa-upload"></div> &nbsp;&nbsp; 上傳相片
                                     </div>
-                                </a> <a data-toggle="tab" href="#menu2" id="tab2" class="tabs list-group-item active1">
+                                </a>
+                                <a data-toggle="tab" href="#menu3" id="tab3" class="tabs list-group-item bg-light">
                                     <div class="list-div my-2">
-                                        <div class="fa fa-credit-card"></div> &nbsp;&nbsp; Card
+                                        <div class="fa fa-image"></div> &nbsp;&nbsp;&nbsp; 瀏覽相簿 <span id="new-label">NEW</span>
                                     </div>
-                                </a> <a data-toggle="tab" href="#menu3" id="tab3" class="tabs list-group-item bg-light">
-                                    <div class="list-div my-2">
-                                        <div class="fa fa-qrcode"></div> &nbsp;&nbsp;&nbsp; Visa QR <span id="new-label">NEW</span>
-                                    </div>
-                                </a> </div>
-                        </div> <!-- Page Content -->
+                                </a>
+                            </div>
+                        </div>
+                        <!-- Page Content -->
                         <div id="page-content-wrapper">
                             <div class="row pt-3" id="border-btm">
                                 <div class="col-4"> <button class="btn btn-success mt-4 ml-3 mb-3" id="menu-toggle">
@@ -50,52 +50,20 @@
                                 <div class="col-8">
                                     <div class="row justify-content-right">
                                         <div class="col-12">
-                                            <p class="mb-0 mr-4 mt-4 text-right">customer@email.com</p>
+                                            <p class="mb-0 mr-4 mt-4 text-right">luckyowo@email.com&nbsp;</p>
                                         </div>
                                     </div>
                                     <div class="row justify-content-right">
                                         <div class="col-12">
-                                            <p class="mb-0 mr-4 text-right">Pay <span class="top-highlight">$ 100</span> </p>
+                                            <p class="mb-0 mr-4 text-right">小額贊助開發者<span class="top-highlight">$ 60&nbsp;</span> </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="row justify-content-center">
-                                <div class="text-center" id="test">Pay</div>
+                                <div class="text-center" id="test">喵</div>
                             </div>
                             <div class="tab-content">
-                                <div id="menu1" class="tab-pane">
-                                    <div class="row justify-content-center">
-                                        <div class="col-11">
-                                            <div class="form-card">
-                                                <h3 class="mt-0 mb-4 text-center">Enter bank details to pay</h3>
-                                                <form onsubmit="event.preventDefault()">
-                                                    <div class="row">
-                                                        <div class="col-12">
-                                                            <div class="input-group"> <input type="text" id="bk_nm" placeholder="BBB Bank"> <label>BANK NAME</label> </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col-12">
-                                                            <div class="input-group"> <input type="text" name="ben_nm" id="ben-nm" placeholder="John Smith"> <label>BENEFICIARY NAME</label> </div>
-                                                        </div>
-                                                        <div class="col-12">
-                                                            <div class="input-group"> <input type="text" name="scode" placeholder="ABCDAB1S" class="placeicon" minlength="8" maxlength="11"> <label>SWIFT CODE</label> </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col-md-12"> <input type="submit" value="Pay $ 100" class="btn btn-success placeicon"> </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col-md-12">
-                                                            <p class="text-center mb-5" id="below-btn"><a href="#">Use a test card</a></p>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
                                 <div id="menu2" class="tab-pane in active">
                                     <div class="row justify-content-center">
                                         <div class="col-11">
@@ -131,7 +99,7 @@
                                 <div id="menu3" class="tab-pane">
                                     <div class="row justify-content-center">
                                         <div class="col-11">
-                                            <h3 class="mt-0 mb-4 text-center">Scan the QR code to pay</h3>
+                                            <h3 class="mt-0 mb-4 text-center">瀏覽已上傳相片</h3>
                                             <div class="row justify-content-center">
                                                 <div id="qr"> <img src="https://i.imgur.com/DD4Npfw.jpg" width="200px" height="200px"> </div>
                                             </div>
