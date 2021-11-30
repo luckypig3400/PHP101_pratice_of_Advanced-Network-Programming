1fee930991363349506c71cdba2c1d9f13c376e6
郵差 <twHenryYang@gmail.com>
2021-11-30 17:00:54 +0800

    Auto retract sidebar when click any sidebar option

--- Week10_FileUpload_and_ImageRWD/script.js
+++ Week10_FileUpload_and_ImageRWD/script.js
@@ -11,17 +11,20 @@ $(document).ready(function () {
         $(".tabs").addClass("bg-light");
         $("#tab1").addClass("active1");
         $("#tab1").removeClass("bg-light");
+        $("#wrapper").removeClass("toggled");
     });
     $("#tab2").click(function () {
         $(".tabs").removeClass("active1");
         $(".tabs").addClass("bg-light");
         $("#tab2").addClass("active1");
         $("#tab2").removeClass("bg-light");
+        $("#wrapper").removeClass("toggled");
     });
     $("#tab3").click(function () {
         $(".tabs").removeClass("active1");
         $(".tabs").addClass("bg-light");
         $("#tab3").addClass("active1");
         $("#tab3").removeClass("bg-light");
+        $("#wrapper").removeClass("toggled");
     });
 })
