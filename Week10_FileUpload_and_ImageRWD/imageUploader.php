a54fdcaa8703738cf30eafe6631cd12354ec5b26
郵差 <twHenryYang@gmail.com>
2021-12-06 12:08:01 +0800

    Swap upload & photos tab

--- Week10_FileUpload_and_ImageRWD/imageUploader.php
+++ Week10_FileUpload_and_ImageRWD/imageUploader.php
@@ -1,4 +1,5 @@
 <?php
+// https://www.w3schools.com/PHP/php_file_upload.asp
 $target_dir = "./img/";
 $target_file = $target_dir . basename($_FILES["uploadImage"]["name"]);
 $uploadOK = 1;
@@ -81,12 +82,12 @@ if ($uploadOK == 0) {
                             <div class="list-group list-group-flush">
                                 <a data-toggle="tab" href="#menu2" id="tab2" class="tabs list-group-item active1">
                                     <div class="list-div my-2">
-                                        <div class="fa fa-upload"></div> &nbsp;&nbsp; 上傳相片
+                                        <div class="fa fa-image"></div> &nbsp;&nbsp;&nbsp; 瀏覽相簿 <span id="new-label">NEW</span>
                                     </div>
                                 </a>
                                 <a data-toggle="tab" href="#menu3" id="tab3" class="tabs list-group-item bg-light">
                                     <div class="list-div my-2">
-                                        <div class="fa fa-image"></div> &nbsp;&nbsp;&nbsp; 瀏覽相簿 <span id="new-label">NEW</span>
+                                        <div class="fa fa-upload"></div> &nbsp;&nbsp; 上傳相片
                                     </div>
                                 </a>
                             </div>
@@ -119,6 +120,16 @@ if ($uploadOK == 0) {
                                 <div id="menu2" class="tab-pane in active">
                                     <div class="row justify-content-center">
                                         <div class="col-11">
+                                            <h3 class="mt-0 mb-4 text-center">瀏覽已上傳相片</h3>
+                                            <div class="row justify-content-center">
+                                                <div id="qr"> <img src="https://i.imgur.com/DD4Npfw.jpg" width="200px" height="200px"> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="menu3" class="tab-pane">
+                                    <div class="row justify-content-center">
+                                        <div class="col-11">
                                             <div class="form-card">
                                                 <h3 class="mt-0 mb-4 text-center">選擇想要上傳的檔案</h3>
                                                 <form enctype="multipart/form-data" method="post" action="./imageUploader.php">
@@ -130,7 +141,7 @@ if ($uploadOK == 0) {
                                                         </div>
                                                     </div>
                                                     <div class="row">
-                                                        <div class="col-md-12"> <input type="submit" value="Upload Image" class="btn btn-success placeicon" name="imgSubmit"> </div>
+                                                        <div class="col-md-12"> <input id="uploadBtn" type="submit" value="Upload Image" class="btn btn-success placeicon" name="imgSubmit"> </div>
                                                     </div>
                                                     <div class="row">
                                                         <div class="col-md-12">
@@ -138,16 +149,6 @@ if ($uploadOK == 0) {
                                                         </div>
                                                     </div>
                                                 </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div id="menu3" class="tab-pane">
-                                    <div class="row justify-content-center">
-                                        <div class="col-11">
-                                            <h3 class="mt-0 mb-4 text-center">瀏覽已上傳相片</h3>
-                                            <div class="row justify-content-center">
-                                                <div id="qr"> <img src="https://i.imgur.com/DD4Npfw.jpg" width="200px" height="200px"> </div>
                                             </div>
                                         </div>
                                     </div>
