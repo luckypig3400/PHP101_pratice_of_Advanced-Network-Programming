08e7b4e57b145b172414eaca307222e806fba669
郵差 <twHenryYang@gmail.com>
2021-12-22 05:51:33 +0800

    finish Q1 process special characters

--- Week13_Web_Security/01.php
+++ Week13_Web_Security/01.php
@@ -7,17 +7,24 @@
 ?>
 
 <html>
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Disable HTML syntax</title>
 </head>
+
 <body>
 
-<?php
-	$comment = (isset($_POST["comment"]))?$_POST["comment"]:"";
+	<?php
+	$comment = (isset($_POST["comment"])) ? $_POST["comment"] : "";
+
+	$comment = htmlspecialchars($comment); // 轉換&"><'這些特殊符號
+	// https://www.w3schools.com/php/func_string_htmlspecialchars.asp
+	$comment = nl2br($comment); // 換行轉<br>
 
 	echo $comment;
-?>
+	?>
 
 </body>
+
 </html>
