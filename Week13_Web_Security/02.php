cdf7998a9205562a7a449198b6a7d8bd787778ed
郵差 <twHenryYang@gmail.com>
2021-12-22 05:58:40 +0800

    format files

--- Week13_Web_Security/02.php
+++ Week13_Web_Security/02.php
@@ -3,20 +3,22 @@
 	解決SQL漏洞
 */
 
-	$hostname = "localhost";	/* MySQL的主機名稱 */
-	$username = "root";			/* MySQL的使用者名稱 */
-	$password = "";				/* MySQL的使用者密碼 */
-	$database = "w13";			/*資料庫名稱*/
+$hostname = "localhost";	/* MySQL的主機名稱 */
+$username = "root";			/* MySQL的使用者名稱 */
+$password = "";				/* MySQL的使用者密碼 */
+$database = "w13";			/*資料庫名稱*/
 ?>
 
 <html>
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>SQL injection</title>
 </head>
+
 <body>
 
-<?php
+	<?php
 	if ((isset($_POST["username"])) && (isset($_POST["password"])))	// 用isset()檢查變數是否有值(非NULL)
 	{
 		// 接收使用者所傳送之帳號與密碼
@@ -24,35 +26,29 @@
 		$pwd  = $_POST["password"];
 
 		// 建立與MySQL資料庫的連線
-		$link = new PDO('mysql:host='.$hostname.';dbname='.$database.';charset=utf8', $username, $password);
+		$link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
 
 		// 用SQL語法呼叫mysql_query()
-		$query = "SELECT * FROM `person` WHERE `user`='" . $user . "' AND `pwd`='" . $pwd . "'";
+		$query = "SELECT * FROM `fperson` WHERE `user`='" . $user . "' AND `pwd`='" . $pwd . "'";
 		$result = $link->query($query);
 
 		// 找到符合的資料
 		$rows = $result->fetchAll();
 		$rowCount = count($rows);
-		if ($rowCount > 0)
-		{
+		if ($rowCount > 0) {
 			echo "Login Success.<br />\n";
-			foreach ($rows as $row)
-			{
+			foreach ($rows as $row) {
 				// 在此例中，$row中有$row["Time"]與$row["Description"]兩欄位的值。
-				echo $row["user"]."\t".$row["pwd"]."<br />\n";
+				echo $row["user"] . "\t" . $row["pwd"] . "<br />\n";
 			}
-		}
-		else
-		{
+		} else {
 			echo "Login Failure.";
 		}
-
-	}
-	else
-	{
+	} else {
 		echo "Error (Empty)";
 	}
-?>
+	?>
 
 </body>
+
 </html>
--- Week13_Web_Security/index.php
+++ Week13_Web_Security/index.php
@@ -9,24 +9,29 @@
 ?>
 
 <html>
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>SQL injection</title>
 </head>
+
 <body>
-顯示以下輸入的內容，並且：<br />
- 1. 有兩行以上時需換行<br />
- 2. 禁止HTML語法<br />
+	<h3>Q1:顯示特殊字元</h3>
+	顯示以下輸入的內容，並且：<br />
+	1. 有兩行以上時需換行<br />
+	2. 禁止HTML語法<br />
 	<form action="01.php" method="post">
 		<textarea name="comment"></textarea>
 		<input type="submit" value="顯示" />
 	</form><br /><br /><br />
 
-解決SQL漏洞：<br />
+	<h3>防止SQL Injection</h3>
+	解決SQL漏洞：
 	<form action="02.php" method="post">
 		帳號 ：<input type="text" name="username" /><br />
 		密碼 ：<input type="password" name="password" /><br />
 		<input type="submit" value="登入" />
 	</form>
 </body>
+
 </html>
