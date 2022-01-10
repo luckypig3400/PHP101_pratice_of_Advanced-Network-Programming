9366c7316dad986e7c6e076b407f1cae7ff076c3
郵差 <twHenryYang@gmail.com>
2022-01-10 15:09:37 +0800

    Update W16notes.php

--- Week16_selfSite_with_Wordpress/W16notes.php
+++ Week16_selfSite_with_Wordpress/W16notes.php
@@ -6,26 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wordpress練習 架設於Docker container內</title>
+    <link rel="stylesheet" href="../general_style.css">
 </head>
 
 <body>
-    <h3>練習題 (4選2檢查)</h3>
-    <p>
-        ● 修改(或自行新增設計)網頁最上面的圖片，使美
-        化之。
-        ● 修改網頁最下面的資訊，隱藏所有與你網站無關
-        的資訊(例如原有LOGO、版權聲明等)，使資訊
-        個人化。
-        ● 於適當處加入一北護資管的廣告(含連結)
-        以上修改，需包含所有頁面(或大部分頁面)
-        ● 經營一網站。
-    </p>
+    <div class="container">
+        <h3>練習題 (4選2檢查)</h3>
+        <div class="row">
+            <p>
+                ● 修改(或自行新增設計)網頁最上面的圖片，使美化之。<br>
+                ● 修改網頁最下面的資訊，隱藏所有與你網站無關的資訊(例如原有LOGO、版權聲明等)，使資訊個人化。<br>
+                ● 於適當處加入一北護資管的廣告(含連結)以上修改，需包含所有頁面(或大部分頁面)<br>
+                ● 經營一網站。<br>
+            </p>
+        </div>
+        
+        <h3>Docker筆記</h3>
+        <div class="row">
+            <p>
+                <a href="https://hub.docker.com/r/tomsik68/xampp/">Xampp Docker Image 照著指令pull下來即可</a><br>
+                <a href="https://github.com/tomsik68/docker-xampp">Xampp Image作者的Github</a><br>
 
-    <h3>Docker筆記</h3>
-    <p>
-        <a href="https://hub.docker.com/r/tomsik68/xampp/">Xampp Docker Image 照著指令pull下來即可</a>
-        <a href="https://github.com/tomsik68/docker-xampp">Xampp Image作者的Github</a><br>
-    </p>
+                <a href="https://gist.github.com/BFTrick/11294357">Wordpress下載與安裝</a><br>
+            </p>
+        </div>
+
+    </div>
 </body>
 
 </html>
